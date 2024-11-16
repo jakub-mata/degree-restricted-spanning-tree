a7e78eb160454308571344b4ba2bcc999d6043eb
jakubmata <jakub@pop-os.localdomain>
2024-11-16 18:31:33 +0100

    Reflexivity and symmetry confict resolved

--- encode.py
+++ encode.py
@@ -11,12 +11,12 @@ def encode(input_matrix: list[list[int]], degree_constraint: int):
     Var space:
         - e(i,j): if an edge {i,j} is in the original graph
         - s(i,j): if an edge {i,j} is in the spanning tree
-        - f(i,j): if there is a flow from vertex i to j, ensuring connectivity
-        - o(i,j): given a root, if vertex i is above j in a typical top-to-bottom structure
+        - o(i,j): given a root, if vertex i is above j in a typical top-to-bottom tree structure
+        - r(i,j): if there exists a path between i and j (reachability)
     '''
 
     VERTEX_AMOUNT: int = len(input_matrix)
-    var_count: int = 3 * (VERTEX_AMOUNT * VERTEX_AMOUNT)
+    var_count: int = 4 * (VERTEX_AMOUNT * VERTEX_AMOUNT)
 
     #construct graph
     for i in range(VERTEX_AMOUNT):
@@ -34,7 +34,7 @@ def encode(input_matrix: list[list[int]], degree_constraint: int):
 
     #follows graph input
     for i in range(VERTEX_AMOUNT):
-        for j in range(i, VERTEX_AMOUNT):
+        for j in range(VERTEX_AMOUNT):
             cnf.append([-spanning_tree_edge_var(i, j, VERTEX_AMOUNT), original_edge_var(i, j, VERTEX_AMOUNT), 0])
 
     #degree constraint
@@ -54,23 +54,26 @@ def encode(input_matrix: list[list[int]], degree_constraint: int):
     for j in range(VERTEX_AMOUNT):
         cnf.append([order_var(0,j, VERTEX_AMOUNT), 0])
     #order reflexivity
-    for j in range(VERTEX_AMOUNT):
-        cnf.append([order_var(j, j, VERTEX_AMOUNT), 0])
-    #order_symmetry
+    for i in range(VERTEX_AMOUNT):
+        cnf.append([order_var(i, i, VERTEX_AMOUNT), 0])
+    #order_antisymmetry
     for i in range(VERTEX_AMOUNT):
         for j in range(VERTEX_AMOUNT):
+            if j == i: #breaks reflexivity
+                continue
             cnf.append([order_var(i,j, VERTEX_AMOUNT), -order_var(j,i, VERTEX_AMOUNT), 0])
-            cnf.append([-order_var(i,j, VERTEX_AMOUNT), -order_var(j,i,VERTEX_AMOUNT), 0])
+            cnf.append([-order_var(i,j, VERTEX_AMOUNT), order_var(j,i,VERTEX_AMOUNT), 0])
     #order transitivity (through edges and order itself)
     for i in range(VERTEX_AMOUNT):
         for j in range(VERTEX_AMOUNT):
             for k in range(VERTEX_AMOUNT):
-                cnf.append([-order_var(i, j, VERTEX_AMOUNT), order_var(i, k, VERTEX_AMOUNT), 0])
-                cnf.append([-order_var(j, k, VERTEX_AMOUNT), order_var(i, k, VERTEX_AMOUNT), 0])
-                cnf.append([-spanning_tree_edge_var(j, k, VERTEX_AMOUNT), order_var(i, k, VERTEX_AMOUNT), 0])
+                if i == j or j == k or i == k:
+                    continue
+                cnf.append([-order_var(i,j,VERTEX_AMOUNT),-order_var(j,k,VERTEX_AMOUNT),order_var(i,k,VERTEX_AMOUNT), 0])
+                cnf.append([-order_var(i,j,VERTEX_AMOUNT),-spanning_tree_edge_var(j,k,VERTEX_AMOUNT),order_var(i,k,VERTEX_AMOUNT), 0])
 
-    #CONNECTIVITY (follows from acyclicity above and |E|=|V|-1)
-    encoding_helpers.encode_edge_amount(cnf, encoding_helpers.create_edge_subsets(VERTEX_AMOUNT - 1))
+    #REACHABILITY
+    encoding_helpers.encode_edge_count(cnf, VERTEX_AMOUNT)
 
 
     return cnf, var_count
